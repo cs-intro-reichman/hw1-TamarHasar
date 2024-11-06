142a750f32389d529c1b40a384b41f4b49f8844d
TamarHasar <tamar.hasar@post.runi.ac.il>
2024-11-06 18:28:55 +0200

    homework drafts

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,22 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int lim = Integer.parseInt(args[0]);
+		int a = (int) (Math.random() * lim);
+		int b = (int) (Math.random() * lim);
+		int c = (int) (Math.random() * lim);
+
+		System.out.println(a + " " + b + " " + c);
+
+		int sum = a + b + c;
+
+		int min = Math.min(a, b);
+		int min_final = Math.min(min,c);
+		int max = Math.max(a, b);
+		int max_final = Math.max(max,c);
+
+		int mid = sum - min_final - max_final;
+
+		System.out.println(min_final + " " + mid + " " + max_final);
 	}
 }
--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,16 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
+		//names of diners
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+		int cost = Integer.parseInt(args[3]); //full amount needed to pay
+
+		double splitAmount = Math.ceil((double)cost/3);
+
+		System.out.println("Dear " + name1 + ", " + name2 + ", and " 
+							+ name3 + ": pay " + splitAmount + " Shekels each.");
+	  
 	}
 }
